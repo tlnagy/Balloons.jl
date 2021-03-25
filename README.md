254cac66b31c19fa76fdefd2ab58840be1b7436a
Tamas Nagy <github@tamasnagy.com>
2021-03-25 15:49:35 -0700

    move over Packbits inflation code from TiffImages.jl

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
-# Balloons
+# 🎈 Balloons.jl
+
+Make your data as light as air
 
 [![Stable](https://img.shields.io/badge/docs-stable-blue.svg)](https://tlnagy.github.io/Balloons.jl/stable)
 [![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://tlnagy.github.io/Balloons.jl/dev)
 [![Build Status](https://github.com/tlnagy/Balloons.jl/workflows/CI/badge.svg)](https://github.com/tlnagy/Balloons.jl/actions)
 [![Coverage](https://codecov.io/gh/tlnagy/Balloons.jl/branch/master/graph/badge.svg)](https://codecov.io/gh/tlnagy/Balloons.jl)
+
+### Features
+
+Inflate and deflate your data with the following common compression algorithms
+implemented in Julia:
+
+- Packbits
+
+### Usage
+
+TBD
--- src/Balloons.jl
+++ src/Balloons.jl
@@ -1,5 +1,35 @@
 module Balloons
 
-# Write your package code here.
+export deflate, inflate!
+
+abstract type CompressionAlgorithm end
+
+"""
+    Packbits Compression
+
+A simple byte-oriented run-length encoding as defined by the TIFF Spec 6.0
+"""
+struct Packbits <: CompressionAlgorithm end
+
+inflate!(pb::Packbits, input, output) = inflate!(pb, reinterpret(UInt8, input), reinterpret(UInt8, output))
+
+function inflate!(::Packbits, input::Vector{UInt8}, output::Vector{UInt8})
+    ipos, opos = 1, 1
+
+    while opos <= length(output)
+        n = reinterpret(Int8, input[ipos])
+        ipos += 1
+        if 0 <= n <= 127
+            output[opos:opos+n-1] = view(input, ipos:ipos+n-1)
+            ipos += n
+            opos += n
+        elseif -127 <= n <= -1
+            nxt = input[ipos]
+            output[opos:(opos-n-1)] .= nxt
+            opos += -n
+            ipos += 1
+        end
+    end
+end
 
 end
