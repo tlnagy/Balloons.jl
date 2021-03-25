6922e2d51e61c253dec6b1136ef71373bbbf8f5e
Tamas Nagy <tamas@tamasnagy.com>
2021-03-25 16:22:39 -0700

    initial stab at a variable-width LZW decoder

--- src/Balloons.jl
+++ src/Balloons.jl
@@ -32,4 +32,106 @@ function inflate!(::Packbits, input::AbstractVector{UInt8}, output::AbstractVect
     end
 end
 
+struct LZW <: CompressionAlgorithm end
+
+const LZW_CLEARCODE = UInt32(256)
+const LZW_ENDCODE = UInt32(257)
+const LZW_CODEWIDTH = 9
+
+mask(t::Type{T}, p::Integer, l::Integer) where {T} = (typemax(T) << (sizeof(T)*8-l)) >> p
+
+extract(packed::T, p::Integer, l::Integer) where {T} = (packed & mask(T, p, l)) >> (sizeof(T)*8 - p - l)
+
+"""
+    stuff(v, a, loc)
+
+Stuff bits of `a` into 32-bit unsigned integer `v` at a given byte location `loc`
+
+```jldoctest
+julia> Balloons.stuff(zero(UInt32), 0xff, 1)
+0xff000000
+
+julia> Balloons.stuff(zero(UInt32), 0xff, 4)
+0x000000ff
+```
+"""
+function stuff(v::UInt32, a::UInt8, loc::T) where {T <: Integer}
+    @assert 0 < loc <= 4
+    v += (zero(UInt32) + a) << ((4-loc)*8)
+end
+
+function initdict!(dict)
+    empty!(dict)
+
+    for i in 0:255
+        dict[i] = UInt8[i]
+    end
+    dict[LZW_CLEARCODE] = UInt8[0]
+    dict[LZW_ENDCODE] = UInt8[0]
+end
+
+function nextchar(input, ρ, bitwidth)
+    i = ρ÷8+1
+    v = stuff(zero(UInt32), input[i], 1)
+
+    for j in 1:min(3, length(input)-i)
+        v = stuff(v, input[i+j], j+1)
+    end
+    
+    extract(v, mod(ρ, 8), bitwidth)
+end
+
+function inflate(::LZW, input::Vector{UInt8})
+    bitwidth = 9
+
+    ρ = 0 # position in bits
+
+    output = IOBuffer()
+
+    dict = Dict{UInt32, Array{UInt8}}()
+    k = LZW_CLEARCODE
+
+    entry = UInt8[]
+    prev_entry = UInt8[]
+    
+    while true
+        # increase bitwidth if we're out of space
+        if length(dict) >= 2^bitwidth - 1
+            bitwidth += 1
+        end
+
+        k = nextchar(input, ρ, bitwidth)
+        if k == LZW_ENDCODE
+            break
+        end
+        
+        if k == LZW_CLEARCODE
+            initdict!(dict)
+            sz = length(dict)
+
+            ρ += bitwidth
+            k = nextchar(input, ρ, bitwidth)
+            (k == LZW_ENDCODE) && break
+            entry = dict[k]
+            write(output, entry)
+            prev_entry = entry
+        else
+            if haskey(dict, k)
+                entry = dict[k]
+            else
+                entry = vcat(prev_entry, first(prev_entry))
+            end
+
+            write(output, entry)
+            dict[length(dict)] = vcat(prev_entry, first(entry))
+            prev_entry = entry
+        end
+
+        ρ += bitwidth
+    end
+
+    seekstart(output)
+    readavailable(output)
+end
+
 end
